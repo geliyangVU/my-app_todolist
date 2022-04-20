b4c0adc9f39a145cbdb472128ed0ca5c439583c4
Geli Yang <gyang5@villanova.edu>
2022-04-20 14:47:50 -0400

    start to do lis
    start to do list from template

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function TodoItem(){
+    return(
+        <div className="todo-item">
+        <input type="checkbox"/>
+        <p>Placeholder text</p>
+        </div>
+    )
+
+}
+
+export default TodoItem
